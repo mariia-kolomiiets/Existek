4c79e6c8f7476bed37372d7dadfa1897985fb120
Mariia_Kolomiiets <49612039+Mariia-Braflowski@users.noreply.github.com>
2021-08-14 15:58:05 +0300

    add first ad second homeworks

---
+++ KolomiietsM_HomeWork1/KolomiietsM_HomeWork1/PublicService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolomiietsM_HomeWork1
+{
+    public class PublicService
+    {
+        public void publish()
+        {
+            Console.WriteLine("Publishing.");
+        }
+    }
+}
