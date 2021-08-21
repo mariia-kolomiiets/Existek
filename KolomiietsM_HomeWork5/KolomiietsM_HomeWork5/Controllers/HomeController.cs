75a91bb2810d7c086ab9eb21a45020fd696c0d42
Mariia_Kolomiiets <54811905+mariia-kolomiiets@users.noreply.github.com>
2021-08-21 10:47:33 +0300

    HW5 - create controllers

---
+++ KolomiietsM_HomeWork5/KolomiietsM_HomeWork5/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolomiietsM_HomeWork5.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task Get(long id)
+        {
+            await Response.WriteAsync("Get success!");
+        }
+
+        [HttpGet]
+        public async Task Get(string user, int count)
+        {
+            await Response.WriteAsync("Get with 2 params success!");
+        }
+
+        [HttpPost]
+        public async Task Post(string email, int card)
+        {
+            await Response.WriteAsync("Post with 2 params success!");
+        }
+
+        [HttpPost]
+        public async Task Post(string phone, int card, double sum)
+        {
+            await Response.WriteAsync("Post with 3 params success!");
+        }
+
+        [HttpPut]
+        public async Task Put(long id, string user, string email, string phone, int card)
+        {
+            await Response.WriteAsync("Put success!");
+        }
+
+        [HttpDelete]
+        public async Task Delete(long id)
+        {
+            await Response.WriteAsync("Delete success!");
+        }
+    }
+}
